4e77b5d49e027f4c3c835849e83082b938ddde8f
m-artarello <matheus1.martarello@gmail.com>
2024-06-19 20:13:10 -0300

    Criação do projeto de front-end

--- server/src/main/java/br/edu/utfpr/pb/tads/server/security/WebSecurity.java
+++ server/src/main/java/br/edu/utfpr/pb/tads/server/security/WebSecurity.java
@@ -16,6 +16,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Component
 @EnableWebSecurity
@@ -48,7 +53,7 @@ public class WebSecurity {
 
         http.exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(authenticationEntryPoint));
 
-        http.cors(AbstractHttpConfigurer::disable);
+        http.cors(cors -> corsConfigurationSource());
 
         http.authorizeHttpRequests((authorize) ->
                 authorize.requestMatchers(HttpMethod.POST, "/usuario/**").permitAll()
@@ -69,6 +74,25 @@ public class WebSecurity {
     @Bean
     public PasswordEncoder passwordEncoder (){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // Lista das origens autorizadas, no nosso caso que iremos rodar a aplicação localmente o * poderia ser trocado
+        // por: http://localhost:porta, em que :porta será a porta em que a aplicação cliente será executada
+        configuration.setAllowedOrigins(List.of("*"));
+        // Lista dos métodos HTTP autorizados
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT"));
+        // Lista dos Headers autorizados, o Authorization será o header que iremos utilizar para transferir o Token
+        configuration.setAllowedHeaders(List.of("Authorization","x-xsrf-token",
+                "Access-Control-Allow-Headers", "Origin",
+                "Accept", "X-Requested-With", "Content-Type",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers", "Auth-Id-Token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
