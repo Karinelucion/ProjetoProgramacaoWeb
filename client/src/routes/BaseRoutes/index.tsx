22e05abfe7b732b538c347343b1b1ef61800fb72
Karinelucion <karinelucion04@gmail.com>
2024-06-29 17:49:36 -0300

    "ajustes em classes de login"

--- client/src/routes/BaseRoutes/index.tsx
+++ client/src/routes/BaseRoutes/index.tsx
@@ -1,16 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-// import { CategoryFormPage } from "@/pages/CategoryFormPage";
 import { ListagemDeProdutos } from "@/pages/ListagemDeProdutosPage";
 import { AuthenticatedRoutes } from "../AuthenticatedRoutes";
-// import { ProductFormPage } from "@/pages/ProductFormPage";
-// import { ProductListPageV2 } from "@/pages/ProductListPageV2";
-// import { ProductFormPageV2 } from "@/pages/ProductFormPageV2";
-// import { LoginPage } from "@/pages/LoginPage";
- import { CadastroUsuario } from "@/pages/CadastroUsuarioPage";
+import { ProdutosDetalhes } from "@/pages/ProdutoDetalhesPage";
+import { CadastroUsuario } from "@/pages/CadastroUsuarioPage";
 import { LoginPage } from "@/pages/LoginPage";
 import { HomePage } from "@/pages/HomePage";
-// import { AuthenticatedRoutes } from "../AuthenticatedRoutes";
-// import { HomePage } from "@/pages/HomePage";
 import { ProdutosPorCategoria } from "@/pages/ProdutosPorCategoriaPage";
 
 export function BaseRoutes() {
@@ -24,6 +18,7 @@ export function BaseRoutes() {
         <Route path="/" element={<HomePage />} /> 
         <Route path="/produtos" element={<ListagemDeProdutos />} />
         <Route path="/categorias/:id" element={<ProdutosPorCategoria />} />
+        <Route path="/produtos/:id" element={<ProdutosDetalhes />} />
 
         {/* Protected Routes */}
         <Route element={<AuthenticatedRoutes />}>
